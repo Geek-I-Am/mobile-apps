d5a9cb76a1faefb4330f76adf1f7e2e95ddc34fb
Gary Woodfine <gary.woodfine@threenine.co.uk>
2022-01-05 22:12:52 +0000

    started implementing the app

--- lib/main.dart
+++ lib/main.dart
@@ -20,17 +20,14 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Geekiam',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-      ),
+          visualDensity: VisualDensity.adaptivePlatformDensity,
+          appBarTheme: AppBarTheme(
+              elevation: 4.0,
+              centerTitle: true,
+              backgroundColor: Colors.green,
+              titleTextStyle: TextStyle(color: Colors.white, fontFamily: 'Ubuntu', fontSize: 25)
+          )
+         ),
       home: const MyHomePage(title: 'Geekiam'),
     );
   }
